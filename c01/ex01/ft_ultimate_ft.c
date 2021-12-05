d0e8f37ae754693a62db218471d8d22d4a7b7b08
KBrechin <94875227+KBrechin@users.noreply.github.com>
2021-12-05 10:33:32 +1030

    study the ways

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbrechin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 11:24:46 by kbrechin          #+#    #+#             */
+/*   Updated: 2021/12/04 09:50:58 by kbrechin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int main(void)
+{
+	int someInt;
+	someInt = 69;
+	int *ptr0 = &someInt;
+	int **ptr1 = &ptr0;
+	int ***ptr2 = &ptr1;
+	int ****ptr3 = &ptr2;
+	int *****ptr4 = &ptr3;
+	int ******ptr5 = &ptr4; 
+	int *******ptr6 = &ptr5;
+	int ********ptr7 = &ptr6;
+	ft_ultimate_ft(&ptr7);
+	printf("%d", someInt);
+}
+*/
